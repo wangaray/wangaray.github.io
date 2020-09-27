eca2c9a1bced5d376aac76e2af376f6a729b0308
Trubadur <59279181+wangaray@users.noreply.github.com>
2020-09-27 12:02:07 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,34 +1,106 @@
 <?php
+  // Create database connection
+  $db = mysqli_connect("localhost", "root", "root", "image_upload");
 
-$server = $_SERVER['SERVER_ADDR'];
-$username = 'root';
-$password = '';
-dbname = 'images';
-$charset = 'utf8';
-connection = new mysqli($server, $username, $password, $dbname);
+  // Initialize message variable
+  $msg = "";
 
-if($connection->connect_error){
-	die("ijifrjifir".$connection->connect_error);
-}
+  // If upload button is clicked ...
+  if (isset($_POST['upload'])) {
+  	// Get image name
+  	$image = $_FILES['image']['name'];
+  	// Get text
+  	$image_text = mysqli_real_escape_string($db, $_POST['image_text']);
 
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>Пример веб-страницы</title>
-  <style>
-  header {
-	  
-	  text-align: center;
+  	// image file directory
+  	$target = "images/".basename($image);
+
+  	$sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
+  	// execute query
+  	mysqli_query($db, $sql);
+
+  	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+  		$msg = "Image uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload image";
+  	}
   }
-  </style>
- </head>
- <body>
-  <h1>Заголовок</h1>
-  <!-- Комментарий -->
-  <p>Первый абзац.</p>
-  <p>Второй абзац.</p>
- 
- </body>
+  $result = mysqli_query($db, "SELECT * FROM images ORDER BY -ID");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Image Upload</title>
+<style type="text/css">
+   #content{
+   	width: 50%;
+   	margin: 20px auto;
+   	border: 1px solid #fff;
+   }
+   form{
+   	width: 40%;
+   	margin: 100px auto;
+   }
+   form div{
+   	margin-top: 5px;
+   }
+   #img_div{
+   	width: 80%;
+   	padding: 5px;
+   	margin: 15px auto;
+   	border: 2px solid #262626;
+	border-radius: 30px;
+   }
+   #img_div:after{
+   	content: "";
+   	display: block;
+   	clear: both;
+   }
+   img{
+   	float: left;
+   	margin: 0 0 0 3.5%;
+   	width: 27%;
+   	height: auto;
+	border-radius: 5px;
+   }
+   
+   body{
+    background-color: #fff /* Цвет фона веб-страницы */
+   }
+   p {
+    color: black; /* Цвет текста */
+   }
+</style>
+</head>
+<body>
+<form method="POST" action="index.php" enctype="multipart/form-data">
+  	<input type="hidden" name="size" value="1000000">
+  	<div>
+  	  <input type="file" name="image">
+  	</div>
+  	<div>
+      <textarea 
+      	id="text" 
+      	cols="40" 
+      	rows="4" 
+      	name="image_text" 
+      	placeholder="Say something about this image..."></textarea>
+  	</div>
+  	<div>
+  		<button type="submit" name="upload">POST</button>
+  	</div>
+	<form method="POST" action="index.php" enctype="multipart/form-data">
+  </form>
+
+<div id="content">
+  <?php
+    while ($row = mysqli_fetch_array($result)) {
+      echo "<div id='img_div'>";
+      	echo "<img src='images/".$row['image']."' >";
+      	echo "<p>".$row['image_text']."</p>";
+      echo "</div>";
+    }
+  ?>
+</div>
+</body>
 </html>
